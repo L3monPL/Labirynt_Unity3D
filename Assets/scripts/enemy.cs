6ca38c0b7eb7b8afbb7cef1fda5b3cb307083695
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2023-01-04 15:59:35 +0100

    init ramp

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class enemy : MonoBehaviour
 {
@@ -35,6 +36,13 @@ public class enemy : MonoBehaviour
         {
             agent.destination = agent.transform.position;
         }
-        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
